0cea707c88844c8b4235b5940c6a969e27053ede
Roberto <roherool@hotmail.com>
2022-07-14 21:35:01 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+### REACT APPLICATION WITH REDUX
+
+#### Project under development
+
+- This project was created to show the concepts of redux
+- The README is not finished yet
+- The project will receive visual and style improvements
---
+++ src/store/modules/cart/sagas.ts
@@ -0,0 +1,45 @@
+import { all, takeLatest, select, call, put } from "redux-saga/effects";
+import { AxiosResponse } from "axios";
+
+import { ActionTypes } from "./types";
+import { IState } from "../..";
+import {
+  addProductToCartRequest,
+  addProductToCartSuccess,
+  addProductToCartFailure,
+} from "./actions";
+
+import api from "../../../libs/api";
+
+type CheckProductStockRequest = ReturnType<typeof addProductToCartRequest>;
+
+interface IStockResponse {
+  id: number;
+  quantity: number;
+}
+
+function* checkProductStock({ payload }: CheckProductStockRequest) {
+  const { product } = payload;
+
+  const currentQuantity: number = yield select((state: IState) => {
+    return (
+      state.cart.items.find((item) => item.product.id === product.id)
+        ?.quantity ?? 0
+    );
+  });
+
+  const availableStockResponse: AxiosResponse<IStockResponse> = yield call(
+    api.get,
+    `stock/${product.id}`
+  );
+
+  if (availableStockResponse.data.quantity > currentQuantity) {
+    yield put(addProductToCartSuccess(product));
+  } else {
+    yield put(addProductToCartFailure(product.id));
+  }
+}
+
+export default all([
+  takeLatest(ActionTypes.addProductToCartRequest, checkProductStock),
+]);
